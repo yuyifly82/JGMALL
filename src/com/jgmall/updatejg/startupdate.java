3a5d955b37831b71faaeb047cdea05d1691f94cc
yuyifly82 <yuyifly82@outlook.com>
2016-04-26 17:31:38 +0800

    改名了

---
+++ src/com/jgmall/updatejg/startupdate.java
@@ -0,0 +1,30 @@
+package com.jgmall.updatejg;
+
+
+import com.insthub.jgmall.R;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class startupdate extends Activity {
+	@Override 
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState) ;
+		setContentView(R.layout.update_layout);
+	}
+
+}
